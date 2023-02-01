33320cb62866147496c164a94e5a957731b70372
Ponomareva Nina <nina.9412@mail.ru>
2023-02-01 20:07:29 +0300

    feat: implement functionality for theme change

--- client/src/components/UserMenu/UserMenu.tsx
+++ client/src/components/UserMenu/UserMenu.tsx
@@ -1,18 +1,40 @@
 import { useTranslation } from "react-i18next";
 import { Button } from "../UI";
-import { LANGUAGES } from "./types";
+import { LANGUAGES, THEME } from "./types";
+
+const root = document.querySelector(":root") as HTMLElement;
+const currentTheme = localStorage.getItem("theme");
+root.className = currentTheme === THEME.DARK ? THEME.DARK : THEME.LIGHT;
+
+const setThemeLight = () => {
+  localStorage.setItem("theme", THEME.LIGHT);
+  root.className = THEME.LIGHT;
+};
+
+const setThemeDark = () => {
+  localStorage.setItem("theme", THEME.DARK);
+  root.className = THEME.DARK;
+};
 
 export const UserMenu = () => {
   const { t, i18n } = useTranslation();
 
   return (
     <>
-      <p>{t("menu.language")}:</p>
+      <div>{t("menu.language")}:</div>
       <Button btnSize="small" onClick={() => i18n.changeLanguage(LANGUAGES.EN)}>
         {LANGUAGES.EN}
       </Button>
       <Button btnSize="small" onClick={() => i18n.changeLanguage(LANGUAGES.RU)}>
         {LANGUAGES.RU}
+      </Button>
+
+      <div>{t("menu.theme")}:</div>
+      <Button btnSize="small" onClick={() => setThemeLight()}>
+        {THEME.LIGHT}
+      </Button>
+      <Button btnSize="small" onClick={() => setThemeDark()}>
+        {THEME.DARK}
       </Button>
     </>
   );
