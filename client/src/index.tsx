d66f12bb2e8c8bb08f2ab0152ca67bef514346be
Ponomareva Nina <nina.9412@mail.ru>
2023-02-01 21:10:21 +0300

    feat: update common styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./translation/i18n";
 import "./variables.scss";
-import "./index.scss";
+import "./common.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
