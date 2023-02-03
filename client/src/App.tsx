cc08c7081d7683ed116e9efe471952a3969d8a48
Ponomareva Nina <nina.9412@mail.ru>
2023-02-03 18:54:35 +0300

    feat: change router to BrowserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./layout/Layout";
+import { AuthPage } from "./pages/AuthPage/AuthPage";
 import { StartPage } from "./pages/StartPage/StartPage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<StartPage />} />
+        <Route path="auth" element={<AuthPage />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Layout>
