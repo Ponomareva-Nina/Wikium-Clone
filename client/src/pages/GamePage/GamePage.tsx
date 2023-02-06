a932b55f6a32f818e312db853a0bd5e966db4f22
Ponomareva Nina <nina.9412@mail.ru>
2023-02-06 20:06:34 +0300

    feat: add stylesheet for GamePage

--- client/src/pages/GamePage/GamePage.tsx
+++ client/src/pages/GamePage/GamePage.tsx
@@ -1,23 +1,26 @@
 import { useTranslation } from "react-i18next";
 import { GameCategory } from "../../components/GameCategory/GameCategory";
+import styles from "./GamePage.module.scss";
 
 export const GamePage = () => {
   const { t } = useTranslation();
 
   return (
     <>
-      <h1>{t("gamePage.title")}</h1>
+      <h1 className={styles.page_title}>{t("gamePage.title")}</h1>
       <GameCategory
         title={t("gamePage.concentrationTitle")}
         description={t("gamePage.concentrationDescription")}
       >
         <span>TO DO: delete this span after you add GameTeaserCard</span>
       </GameCategory>
+
       <GameCategory title={t("gamePage.memoryTitle")} description={t("gamePage.memoryDescription")}>
-        <span>TO DO: delete this span after you add GameTeaserCard</span>
+        <span>delete this span after you add GameTeaserCard</span>
       </GameCategory>
+
       <GameCategory title={t("gamePage.logicsTitle")} description={t("gamePage.logicsDescription")}>
-        <span>TO DO: delete this span after you add GameTeaserCard</span>
+        <span>delete this span after you add GameTeaserCard</span>
       </GameCategory>
     </>
   );
