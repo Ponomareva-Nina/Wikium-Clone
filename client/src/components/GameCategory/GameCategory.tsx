7853e58435626fafe8cf7ef360bbe8f3f8ba5b52
Ponomareva Nina <nina.9412@mail.ru>
2023-02-07 17:14:27 +0300

    feat: add icons to games page

--- client/src/components/GameCategory/GameCategory.tsx
+++ client/src/components/GameCategory/GameCategory.tsx
@@ -1,20 +1,27 @@
 import { FC, PropsWithChildren } from "react";
 import cn from "classnames";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ReactSVG } from "react-svg";
 import styles from "./GameCategory.module.scss";
 
 interface GameCategoryProps {
   title: string;
   description: string;
+  icon?: string;
 }
 
 export const GameCategory: FC<PropsWithChildren<GameCategoryProps>> = ({
   title,
+  icon = "",
   description,
   children,
 }) => {
   return (
     <section className={cn(styles.category__container)}>
-      <h2 className={cn(styles.title)}>{title}</h2>
+      <h2 className={cn(styles.title)}>
+        <ReactSVG src={icon} />
+        {title}
+      </h2>
       <p>{description}</p>
       <div className={cn(styles.games)}>{children}</div>
     </section>
--- client/src/pages/GamesPage/GamesPage.tsx
+++ client/src/pages/GamesPage/GamesPage.tsx
@@ -3,42 +3,54 @@ import cn from "classnames";
 import { GameCategory } from "../../components/GameCategory/GameCategory";
 import styles from "./GamesPage.module.scss";
 import { GameTeaserCard } from "../../components";
-import colorMatchImg from "../../assets/images/GamePage/color-match.svg";
-import penaltyImg from "../../assets/images/GamePage/penalty.svg";
-import moreAndLessImg from "../../assets/images/GamePage/more-and-less.svg";
+import colorMatchImg from "../../assets/images/GamesPage/color-match.svg";
+import penaltyImg from "../../assets/images/GamesPage/penalty.svg";
+import moreAndLessImg from "../../assets/images/GamesPage/more-and-less.svg";
+import concentration from "../../assets/images/GamesPage/concentration.svg";
+import logics from "../../assets/images/GamesPage/logics.svg";
+import memory from "../../assets/images/GamesPage/memory.svg";
 
 export const GamesPage = () => {
   const { t } = useTranslation();
 
   return (
     <div className={cn("wrapper", styles.page)}>
-      <h1 className={styles.page_title}>{t("gamePage.title")}</h1>
+      <h1 className={styles.page_title}>{t("gamesPage.title")}</h1>
       <GameCategory
-        title={t("gamePage.concentrationTitle")}
-        description={t("gamePage.concentrationDescription")}
+        title={t("gamesPage.concentrationTitle")}
+        icon={concentration}
+        description={t("gamesPage.concentrationDescription")}
       >
         <GameTeaserCard
           id={1}
-          title={t("gamePage.colorMatchGame")}
-          category={t("gamePage.concentrationTitle")}
+          title={t("gamesPage.colorMatchGame")}
+          category={t("gamesPage.concentrationTitle")}
           imgSrc={colorMatchImg}
         />
       </GameCategory>
 
-      <GameCategory title={t("gamePage.memoryTitle")} description={t("gamePage.memoryDescription")}>
+      <GameCategory
+        title={t("gamesPage.memoryTitle")}
+        icon={memory}
+        description={t("gamesPage.memoryDescription")}
+      >
         <GameTeaserCard
           id={2}
-          title={t("gamePage.penaltyGame")}
-          category={t("gamePage.memoryTitle")}
+          title={t("gamesPage.penaltyGame")}
+          category={t("gamesPage.memoryTitle")}
           imgSrc={penaltyImg}
         />
       </GameCategory>
 
-      <GameCategory title={t("gamePage.logicsTitle")} description={t("gamePage.logicsDescription")}>
+      <GameCategory
+        title={t("gamesPage.logicsTitle")}
+        icon={logics}
+        description={t("gamesPage.logicsDescription")}
+      >
         <GameTeaserCard
           id={3}
-          title={t("gamePage.comparisonGame")}
-          category={t("gamePage.logicsTitle")}
+          title={t("gamesPage.comparisonGame")}
+          category={t("gamesPage.logicsTitle")}
           imgSrc={moreAndLessImg}
         />
       </GameCategory>
