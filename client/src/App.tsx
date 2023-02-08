8431199d472536783a6165d32e438f0d9da7634c
Vladimir <v.milashevsky@gmail.com>
2023-02-09 01:18:37 +0300

    add: add statics and accountPage plugs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./layout/Layout";
+import { AccountSettings } from "./pages/AccountSettings/AccountSettings";
 import { AuthPage } from "./pages/AuthPage/AuthPage";
 import { GamePage } from "./pages/GamePage/GamePage";
 import { RegistrationPage } from "./pages/RegistrationPage/RegistrationPage";
 import { StartPage } from "./pages/StartPage/StartPage";
+import { Statistics } from "./pages/Statistics/Statistics";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/games" element={<GamePage />} />
+        <Route path="/stats" element={<Statistics />} />
+        <Route path="/account" element={<AccountSettings />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Layout>
