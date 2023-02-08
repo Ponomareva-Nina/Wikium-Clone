323cc532159c4e8b981e028e61adb28ef6ea50db
Vladimir <v.milashevsky@gmail.com>
2023-02-08 23:46:25 +0300

    feat: add basic implementation footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+import cn from "classnames";
+import { useTranslation } from "react-i18next";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ReactSVG } from "react-svg";
+import { NavigationItem } from "../NavigationList/NavigationItem/NavigationItem";
+import styles from "./Footer.module.scss";
+import rsLogo from "../../assets/images/Footer/rslogo.svg";
+
 export const Footer = () => {
+  const { t } = useTranslation();
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className={cn(styles.footer)}>
+      <ul className={cn(styles.wrapper, styles.footer__wrapper)}>
+        <NavigationItem to="/team">{t("footer.about")}</NavigationItem>
+        <p className={cn(styles.year)}>{new Date().getFullYear()}</p>
+        <NavigationItem to="https://rs.school/" target="_blank">
+          <div className={cn(styles.logo)}>
+            <ReactSVG src={rsLogo} />
+          </div>
+        </NavigationItem>
+      </ul>
     </footer>
   );
 };
