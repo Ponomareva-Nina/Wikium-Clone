794c12e2c4018e16b17e7b53991c4d2436550165
Vladimir <v.milashevsky@gmail.com>
2023-02-09 16:41:09 +0300

    fix: link color, replace Date() to constant

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import { ReactSVG } from "react-svg";
 import { NavigationItem } from "../NavigationList/NavigationItem/NavigationItem";
 import styles from "./Footer.module.scss";
 import rsLogo from "../../assets/images/Footer/rslogo.svg";
+import { RS_LINK, YEAR } from "../../constants/constants";
 
 export const Footer = () => {
   const { t } = useTranslation();
@@ -12,8 +13,8 @@ export const Footer = () => {
     <footer className={cn(styles.footer)}>
       <ul className={cn(styles.wrapper, styles.footer__wrapper)}>
         <NavigationItem to="/team">{t("footer.about")}</NavigationItem>
-        <p className={cn(styles.year)}>{new Date().getFullYear()}</p>
-        <NavigationItem to="https://rs.school/" target="_blank">
+        <p className={cn(styles.year)}>{YEAR}</p>
+        <NavigationItem to={RS_LINK} target="_blank">
           <div className={cn(styles.logo)}>
             <ReactSVG src={rsLogo} />
           </div>
