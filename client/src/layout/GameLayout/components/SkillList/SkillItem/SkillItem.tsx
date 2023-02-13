c4ba130d963aa8c9ae31d49cc3c41628170342cf
Ponomareva Nina <nina.9412@mail.ru>
2023-02-13 18:13:21 +0300

    feat: add translation to skillItem component

--- client/src/layout/GameLayout/components/SkillList/SkillItem/SkillItem.tsx
+++ client/src/layout/GameLayout/components/SkillList/SkillItem/SkillItem.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { Skill } from "../../../../../interfaces/GameInterface";
 import styles from "./SkillItem.module.scss";
 
@@ -7,12 +8,13 @@ interface SkillItemProps {
 }
 
 export const SkillItem: FC<SkillItemProps> = ({ skill }) => {
+  const { t } = useTranslation();
   return (
     <li className={styles.wrapper}>
-      <img className={styles.icon} src={skill.icon} alt={skill.title} />
+      <img className={styles.icon} src={skill.icon} alt={t(skill.title) || "image"} />
       <div className={styles.content}>
-        <div className={styles.title}>{skill.title}</div>
-        <div className={styles.description}>{skill.description}</div>
+        <div className={styles.title}>{t(skill.title)}</div>
+        <div className={styles.description}>{t(skill.description)}</div>
       </div>
     </li>
   );
