0f737035946c9a85c953c54f1524b3c1eb9972b9
Ponomareva Nina <nina.9412@mail.ru>
2023-02-14 16:47:25 +0300

    feat: add translation to info panel

--- client/src/games/RememberOrderGame/components/InfoPanel/InfoPanel.tsx
+++ client/src/games/RememberOrderGame/components/InfoPanel/InfoPanel.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 import { getTimeFromSecond } from "../../../../utils/date.util";
 import { Levels, levelsData } from "../../data";
 import styles from "./InfoPanel.module.scss";
@@ -12,20 +13,21 @@ interface InfoPanelProps {
 const lastLevel = levelsData[Levels.LAST].level;
 
 export const InfoPanel: FC<InfoPanelProps> = ({ timer, level, score }) => {
+  const { t } = useTranslation();
   return (
     <div className={styles.wrapper}>
       <div className={styles.item}>
-        <div className={styles.title}>Время:</div>
+        <div className={styles.title}>{t("gamesData.time")}:</div>
         <div className={styles.value}>{getTimeFromSecond(timer)}</div>
       </div>
       <div className={styles.item}>
-        <div className={styles.title}>Уровень:</div>
+        <div className={styles.title}>{t("gamesData.level")}:</div>
         <div className={styles.value}>
           {level}/{lastLevel}
         </div>
       </div>
       <div className={styles.item}>
-        <div className={styles.title}>Очки:</div>
+        <div className={styles.title}>{t("gamesData.score")}:</div>
         <div className={styles.value}>{score}</div>
       </div>
     </div>
