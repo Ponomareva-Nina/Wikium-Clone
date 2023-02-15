93b6288a755a556a75480e5cb4ef7ae5aea91ec1
Ponomareva Nina <nina.9412@mail.ru>
2023-02-15 17:53:04 +0300

    refactor: change key in map for unique value instead of index

--- client/src/components/GameResults/GameResults.tsx
+++ client/src/components/GameResults/GameResults.tsx
@@ -48,8 +48,10 @@ export const GameResults: FC<GameResultsProps> = ({
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>{t("gameResults.results")}</div>
-      {results.map((result, index) => {
-        return <ResultItem key={index} resultTitle={t(result.title)} resultValue={result.value} />;
+      {results.map((result) => {
+        return (
+          <ResultItem key={result.title} resultTitle={t(result.title)} resultValue={result.value} />
+        );
       })}
       <Button onClick={newGameHandler}>{t("gameResults.playAgain")}</Button>
     </div>
