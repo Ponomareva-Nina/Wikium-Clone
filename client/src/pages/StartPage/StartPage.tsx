a6f49fd1d38a5ba7c2da7520e2a398ab525718c4
Vladimir <v.milashevsky@gmail.com>
2023-02-16 16:39:03 +0300

    feat: init start_page

--- client/src/pages/StartPage/StartPage.tsx
+++ client/src/pages/StartPage/StartPage.tsx
@@ -1,7 +1,14 @@
 import { useTranslation } from "react-i18next";
+import cn from "classnames";
+import styles from "./StartPage.module.scss";
+import { Header } from "../../components";
 
 export const StartPage = () => {
   const { t } = useTranslation();
 
-  return <h1>{t("startPage.title")}</h1>;
+  return (
+    <div className={cn(styles.container)}>
+      <h1>{t("startPage.title")}</h1>
+    </div>
+  );
 };
