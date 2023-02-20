ad940813627463aedb79cc2eb759b89a86e8dad4
Ponomareva Nina <nina.9412@mail.ru>
2023-02-20 13:52:56 +0300

    feat: setup for deploy on gh-pages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./translation/i18n";
 import "./variables.scss";
@@ -9,8 +9,8 @@ import "./common.scss";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
