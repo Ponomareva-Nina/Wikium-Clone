c6526c7921b4315252cf7c660497cf07e85d54e4
Ponomareva Nina <nina.9412@mail.ru>
2023-02-20 20:34:01 +0300

    fix: fix imports

--- client/src/components/UI/index.ts
+++ client/src/components/UI/index.ts
@@ -2,6 +2,5 @@ export { Button } from "./Button/Button";
 export { Input } from "./Input/Input";
 export { Logo } from "./Logo/Logo";
 export { Select } from "./Select/Select";
-export { AccountNav } from "../NavigationList/AccountNav/AccountNav";
 export { AnswerButton } from "./AnswerButton/AnswerButton";
 export { ScreenSaver } from "./ScreenSaver/ScreenSaver";
--- client/src/components/UserMenu/UserMenu.tsx
+++ client/src/components/UserMenu/UserMenu.tsx
@@ -1,7 +1,6 @@
 import { DetailedHTMLProps, FC, HTMLAttributes, PropsWithChildren } from "react";
-import { AccountNav } from "../UI";
+import { AccountNav } from "../NavigationList/AccountNav/AccountNav";
 
-import { THEME } from "./types";
 import styles from "./UserMenu.module.scss";
 import { NavigationList } from "../NavigationList/NavigationList";
 import { useViewport } from "../../utils/useViewport";
